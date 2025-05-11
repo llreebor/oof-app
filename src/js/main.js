bacc5029f6fbca81e3927673e6ee510a4186fd7f
llreebor <timoschuk.roman@gmail.com>
2025-05-11 16:30:52 +0300

    start dashboard page

--- src/js/main.js
+++ src/js/main.js
@@ -26,3 +26,59 @@ function togglePasswordVisibility() {
 }
 
 togglePasswordVisibility()
+
+// Toggle Slidebar visibility
+function toggleSidebarVisibility() {
+	const leftSidebar = document.getElementById("left-sidebar")
+	const leftSidebarTrigger = document.getElementById("left-sidebar-trigger")
+	const rightSidebar = document.getElementById("right-sidebar")
+	const rightSidebarTrigger = document.getElementById("right-sidebar-trigger")
+	if (!leftSidebar) return
+	if (!rightSidebar) return
+
+	leftSidebarTrigger.addEventListener("click", () => {
+		leftSidebar.classList.toggle("closed")
+		leftSidebarTrigger.classList.toggle("active")
+	})
+	rightSidebarTrigger.addEventListener("click", () => {
+		rightSidebar.classList.toggle("closed")
+		rightSidebarTrigger.classList.toggle("active")
+	})
+}
+toggleSidebarVisibility()
+
+// Data selects
+function initializeCustomSelect(selectId, optionsId, selectedOptionId) {
+	const customSelect = document.getElementById(selectId)
+	const selectedOption = document.getElementById(selectedOptionId)
+	const customOptions = document.getElementById(optionsId)
+	const options = customOptions.getElementsByClassName("option")
+
+	customSelect.addEventListener("click", () => {
+		customOptions.classList.toggle("hidden")
+		const arrow = customSelect.querySelector(".arrow svg")
+		arrow.style.transform = customOptions.classList.contains("hidden")
+			? "rotate(0deg)"
+			: "rotate(180deg)"
+	})
+
+	for (let option of options) {
+		option.addEventListener("click", () => {
+			selectedOption.innerText = option.innerText
+			customOptions.classList.add("hidden")
+			const arrow = customSelect.querySelector(".arrow svg")
+			arrow.style.transform = "rotate(0deg)"
+		})
+	}
+
+	document.addEventListener("click", (event) => {
+		const target = event.target
+		if (!customSelect.contains(target) && !customOptions.contains(target)) {
+			customOptions.classList.add("hidden")
+			const arrow = customSelect.querySelector(".arrow svg")
+			arrow.style.transform = "rotate(0deg)"
+		}
+	})
+}
+initializeCustomSelect("select-1", "options-1", "selected-option-1")
+initializeCustomSelect("select-2", "options-2", "selected-option-2")
